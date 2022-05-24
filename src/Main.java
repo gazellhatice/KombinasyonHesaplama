056dc37892c0f46dd4961d2ac9a65f1c56141e95
Hatice GAZEL <98157466+gazellhatice@users.noreply.github.com>
2022-05-24 16:35:15 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int number, group, combination = 1, calculation = 1;
+        System.out.print("Kümenin eleman sayısını giriniz: ");
+        number = scan.nextInt();
+
+        System.out.print("Kaç elemanlı farklı grup oluşturmak istediğinizi giriniz: ");
+        group = scan.nextInt();
+
+        int a = group;
+        int grup = group;
+        int sayi = number;
+
+        for (;a >= 1; a--){
+            combination *= number;
+            number--;
+        }
+
+        int b = group;
+
+        for (;b >= 1;b--){
+            calculation *= group;
+            group--;
+        }
+
+        System.out.println(sayi + "'ın " + grup + "'lı kombinasyonu: " + combination / calculation);
+    }
+}
